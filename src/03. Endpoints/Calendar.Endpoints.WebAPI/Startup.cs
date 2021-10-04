15aff94975266802231e31e008c6a8b021d40a36
bakhtiary <Bakhtiary.amir@hotmail.com>
2021-10-04 08:22:52 +0330

    +edit docker compose

--- src/03. Endpoints/Calendar.Endpoints.WebAPI/Startup.cs
+++ src/03. Endpoints/Calendar.Endpoints.WebAPI/Startup.cs
@@ -44,14 +44,12 @@ namespace Calendar.Endpoints.WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calendar API v1");
+                });
             }
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calendar API v1");
-                
-            });
 
             app.UseMiddleware<SerilogMiddleware>();
 
